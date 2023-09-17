1fd43842b85689aad4ca7813a0532bfe42a4d1eb
M-Uzair-dev <uzairmanan3@gmail.com>
2023-09-17 14:20:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function openLinkupworkinNewTab() {
+  window.open(
+    "https://www.upwork.com/freelancers/~011bbc07af641709b0",
+    "_blank"
+  );
+}
+function openLinkgithubinNewTab() {
+  window.open("https://github.com/M-Uzair-dev", "_blank");
+}
+function openLinkp1inNewTab() {
+  window.open("https://cinemorphia.vercel.app", "_blank");
+}
+function openLinkp2inNewTab() {
+  window.open("https://propertyseekers.vercel.app", "_blank");
+}
+function openLinkg1inNewTab() {
+  window.open("https://github.com/M-Uzair-dev/Cinemorphia", "_blank");
+}
+
+function openLinkg2inNewTab() {
+  window.open("https://github.com/M-Uzair-dev/PropertySeekers", "_blank");
+}
+function openLinkp3inNewTab() {
+  window.open("https://foodfundayorg.vercel.app", "_blank");
+}
+function openLinkg3inNewTab() {
+  window.open("https://github.com/M-Uzair-dev/foodfunday", "_blank");
+}
+function openLinkp4inNewTab() {
+  window.open("https://fitappweb.vercel.app", "_blank");
+}
+function openLinkg4inNewTab() {
+  window.open("https://github.com/M-Uzair-dev/fitapp", "_blank");
+}
